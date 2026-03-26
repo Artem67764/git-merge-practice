1c37e9d80e37eb69b3044b2aef6cee96d4d88c35
KSTU Student <KSTU.Student@unet.kstu.kz>
2026-03-26 10:36:31 +0500

    Добавлены функции умножения и деления

--- calculator.py
+++ calculator.py
@@ -6,3 +6,11 @@ if __name__ == "__main__":
     print("Простой калькулятор")
     print("Сложение 5 + 3 =",add(5, 3))
     print("Вычитание 10 - 4 =", subtract(10, 4))
+def multiply(a, b):
+    return a * b
+def divide (a, b):
+    if b == 0:
+        return "Ошибка: Деление на ноль!"
+    return a / b
+print("Умножение 6 * 7 =", multiply(6, 7))
+print("Деление 15 / 3 =", divide (15, 3))
